99db750d31de4acd9c4eaac6f5e1e3347bf8026f
김상기/호스팅개발팀/EP <sagngki.kim@nhnenter.com>
2024-07-12 11:08:10 +0900

    feat: init

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.querySelector('#btn');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Display an alert when the button is clicked
+    alert('Button clicked!');
+});
